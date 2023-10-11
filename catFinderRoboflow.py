0516f845267ede4c6587dca2846747352c8cb372
Graham Jones <grahamjones139@gmail.com>
2023-10-11 23:28:42 +0100

    Testing using video files running ok

--- catFinderRoboflow.py
+++ catFinderRoboflow.py
@@ -1,14 +1,72 @@
 #!/usr/bin/env python
+import cv2
+import catFinder
 from roboflow import Roboflow
-rf = Roboflow(api_key="qpeWz2ekPrHwb4RM4SK0")
-project = rf.workspace().project("catocam")
-model = project.version(3).model
 
-# infer on a local image
-print(model.predict("fastCatTest.mp4", confidence=50, overlap=30).json())
+class CatFinderRoboflow(catFinder.CatFinder):
+    def __init__(self, settingsObj, debug):
+        super().__init__(settingsObj, debug)
+        rf = Roboflow(api_key=settingsObj['apiKey'])
+        project = rf.workspace().project(settingsObj['projectId'])
+        self.model = project.version(settingsObj['versionId']).model
 
-# visualize your prediction
-model.predict("catTest.jpg", confidence=40, overlap=30).save("prediction.jpg")
+
+    def findCat(self, img):
+        # infer on a local image
+        retObj = self.model.predict(img, confidence=50, overlap=30).json()
+        #print("CatFinderRoboflow.findCat() - retObj=", retObj)
+        foundCat = False
+        for pred in retObj['predictions']:
+            if pred['class']=="Cat" and pred['confidence']>0.5:
+                foundCat = True
+        return(foundCat, retObj)
+
+    def getAnnotatedImage(self, img):
+            '''
+            Returns the image annotated with the detected objects, using the retObj 
+            returned by findCat()
+            '''
+            results = self.model.predict(img, confidence=50, overlap=30).json()
+            #print(results)
+            if len(results['predictions'])>0:
+                for pred in results['predictions']:
+                    print(pred)
+                   
+                    #bounding_box = results[0][0]
+                    #print(bounding_box)
+
+                    #x0, y0, x1, y1 = map(int, bounding_box[:4])
+                    x0 = int(pred['x']-0.5*pred['width'])
+                    y0 = int(pred['y']-0.5*pred['height'])
+                    x1 = int(x0 + pred['width'])
+                    y1 = int(y0 + pred['height'])
+
+                    if x0<0:
+                        x0 = 0
+                    if y0 <0:
+                        y0 = 0
+
+                    print("bbox=(%d, %d, %d, %d)" % (x0,y0,x1,y1))
+                    
+                    cv2.rectangle(img, (x0, y0), (x1, y1), (255,255,0), 10)
+                    cv2.putText(img, "Cat", (x0, y0 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 2)
+
+                    h, w, c = img.shape
+                    print("initial w,h=%d,%d" % (w,h))
+                    wNew = 640
+                    scale = w*1.0/640
+                    hNew = int(h*640/w)
+                    print("new w,h=%d,%d" % (wNew, hNew))
+                    
+                    imgScaled = cv2.resize(img, (wNew, hNew))
+
+                    # Show image
+                    cv2.imshow('Image Frame', imgScaled)
+                    cv2.waitKey(1) # waits 1ms
+                    #cv2.destroyAllWindows() # destroys the window showing imag
 
 # infer on an image hosted elsewhere
 # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
+
+if __name__ == "__main__":
+    print("CatFinderRoboflow.main()");
--- catocam.py
+++ catocam.py
@@ -1,48 +1,129 @@
 #!/usr/bin/env python
+import importlib
 import time
 import cv2
 
+import argparse
 #import catFinder
 import json
 
-infile = open("config.json")
-credObj = json.load(infile)
-print(credObj)
 
-camArr = []
+class CatoCam():
+    def __init__(self, configObj, debug=None):
+        self.debug = False
+        if debug is not None:
+            self.debug = debug
+        else:
+            if "debug" in configObj:
+                self.debug = configObj['debug']
+        print("CatoCam.__init__() - debug=%s" % self.debug)
 
-for cam in credObj["cameras"]:
-    # Tp-Link Tapo camera RTSP stream - stream1 is high quality, stream2 low quality.
-    camArr.append(cv2.VideoCapture(cam["serverUrl"]))
+        self.mModels = []
+        self.configObj = configObj
+        self.loadModels()
 
-FRAME_RATE_REQ = 1 # fps
-FRAME_BATCH_SIZE = 10
-nFrames = 0
-batchStartTime = time.time()
-iterDurationReq = 1.0/FRAME_RATE_REQ
-#cf = catFinder.CatFinder()
-while(cap.isOpened()):
-    iterStartTime = time.time()
-    ret, frame = cap.read()
-    #cv2.imshow('frame', frame)
-    #cf.findCat(frame)
-    nFrames += 1
-    if (nFrames >= FRAME_BATCH_SIZE):
-        tdiff = time.time() - batchStartTime
-        fps = nFrames / tdiff
-        print("%d frames in %.1f sec - %.1f fps" % (nFrames, tdiff, fps))
-        batchStartTime = time.time()
+    def loadModels(self):
+        if not "models" in self.configObj:
+            print("CatoCam.loadModels() - ERROR - configObj does not contain a 'models' element.")
+            raise
+        for modelCfg in self.configObj['models']:
+            moduleId = modelCfg['class'].split('.')[0]
+            classId = modelCfg['class'].split('.')[1]
+            print("Importing Module %s" % moduleId)
+            module = importlib.import_module(moduleId)
+            self.mModels.append((
+                modelCfg['name'],
+                eval("module.%s(modelCfg['settings'], self.debug)" % (classId))))
+        
+        for m in self.mModels:
+            print("Model %s" % m[0])
+
+
+    def analyseImage(self, img):
+        for modelName, modelClass in self.mModels:
+            foundCat, retObj = modelClass.findCat(img)
+            print(modelName, foundCat, retObj)
+            if foundCat:
+                modelClass.getAnnotatedImage(img)
+
+    def testFile(self, testFname, fps = 1):
+        print("CatoCam.testFile() - testFname=%s" % testFname)
+        cap = cv2.VideoCapture(testFname)
+        fileFps = cap.get(cv2.CAP_PROP_FPS)
+        frameSkip = fileFps / fps
+        success = True
+        nFrame = 0
+        while success:
+            success, img = cap.read()
+            nFrame += 1
+            if nFrame >=frameSkip:
+                self.analyseImage(img)
+                nFrame = 0
+
+    def getFrames(self, testFname=None):
+        if testFname is not None:
+            self.testFile(testFname)
+
+        camArr = []
+        for cam in credObj["cameras"]:
+            # Tp-Link Tapo camera RTSP stream - stream1 is high quality, stream2 low quality.
+            camArr.append(cv2.VideoCapture(cam["serverUrl"]))
+
+        FRAME_RATE_REQ = 1 # fps
+        FRAME_BATCH_SIZE = 10
         nFrames = 0
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        batchStartTime = time.time()
+        iterDurationReq = 1.0/FRAME_RATE_REQ
+        #cf = catFinder.CatFinder()
+        while(cap.isOpened()):
+            iterStartTime = time.time()
+            ret, frame = cap.read()
+            #cv2.imshow('frame', frame)
+            #cf.findCat(frame)
+            nFrames += 1
+            if (nFrames >= FRAME_BATCH_SIZE):
+                tdiff = time.time() - batchStartTime
+                fps = nFrames / tdiff
+                print("%d frames in %.1f sec - %.1f fps" % (nFrames, tdiff, fps))
+                batchStartTime = time.time()
+                nFrames = 0
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
 
-    # Reduce frame rate to desired  rate.
-    tnow = time.time()
-    iterDuration = iterStartTime - tnow
-    if (iterDuration < iterDurationReq):
-        time.sleep(iterDurationReq - iterDuration)
+            # Reduce frame rate to desired  rate.
+            tnow = time.time()
+            iterDuration = iterStartTime - tnow
+            if (iterDuration < iterDurationReq):
+                time.sleep(iterDurationReq - iterDuration)
 
-cap.release()
-cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
 
 
+if __name__ == "__main__":
+    print("catocam.main()")
+    parser = argparse.ArgumentParser(description='Detect Cats in Video Streams')
+    parser.add_argument('--config', default="config.json",
+                        help='name of json configuration file')
+    parser.add_argument('--test', default="testFile.mp4",
+                        help='run the system on a test video file rather than live data')
+    #parser.add_argument('--index', action="store_true",
+    #                    help='Re-build index, not all summaries')
+    parser.add_argument('--debug', action="store_true",
+                        help='Write debugging information to screen')
+    
+    argsNamespace = parser.parse_args()
+    args = vars(argsNamespace)
+    print(args)
+
+    infile = open(args['config'])
+    configObj = json.load(infile)
+    print(configObj)
+
+    cc = CatoCam(configObj, debug=args['debug'])
+    if args['test'] is not None:
+        testFname = args['test']
+        print("Testing using file %s" % testFname)
+        cc.getFrames(testFname=testFname)
+    else:
+        print("Monitoring Live Camera Streams")
